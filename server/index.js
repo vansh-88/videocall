784280e2ae8dc9b414b545fd601ac7199e674a17
vansh-88 <vanshgupta7891@gmail.com>
2026-02-04 13:22:47 +0530

    debugged

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ import { Server } from "socket.io";
 
 const io = new Server(3000, {
   cors: {
-    // origin: "https://videocall-beta-gold.vercel.app",
-    origin: "http://localhost:5173",
+    origin: "https://videocall-beta-gold.vercel.app",
+    // origin: "http://localhost:5173",
   },
 });
 
